38ca071c166bbcc96395fb5936d13d3f90e9336a
Calvin Kinateder <calvinkinateder@gmail.com>
2024-03-17 08:33:08 -0400

    dataset made, organized by file?

--- linc/tests.py
+++ linc/tests.py
@@ -1,3 +1,4 @@
 from models import *
 
 # implement ProofWriter ( download script) and test against RandomModel
+# add subsample method that samples a random subset of the data (in the paper it's 360 samples)

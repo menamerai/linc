ec1e484b6c630c8e888a5c23714972ee41e14236
Calvin Kinateder <calvinkinateder@gmail.com>
2024-03-17 08:59:29 -0400

    change setup

--- linc/dataset.py
+++ linc/dataset.py
@@ -23,6 +23,7 @@ class DataSample:
         self.abductions = abductions
         self.filename = filename
         self.answerable = []
+        # TODO - parse each triple, rule, and abduction into FOL
 
         # check if there are any answers, sets to the abduction name
         for k, v in self.abductions.items():
@@ -34,7 +35,7 @@ class DataSample:
             f"DataSample[{self.id}][{self.filename}]\n"
             f" Triples:\n  {pformat(self.triples, indent=1)}\n"
             f" Rules:\n  {pformat(self.rules, indent=1)}\n"
-            f" Abductions:\n  {pformat(self.abductions, indent=1)}"
+            f" Abductions:\n  {pformat(self.abductions, indent=1)}\n"
             f" Answerable: {self.answerable}"
         )
 
@@ -47,50 +48,60 @@ class DataSample:
         return f"DataSample({self.id}, {len(self.answerable)}A, {len(self.triples)}T, {len(self.rules)}R, {len(self.abductions)}AB)"
 
 
+def get_files(path: str, glob_str: str) -> List[str]:
+    """Get all files in a directory matching a glob pattern
+
+    Args:
+        path (str): _description_
+        glob_str (str): _description_
+
+    Returns:
+        List[str]: _description_
+    """
+    path = os.path.normpath(path)  # format the path
+    if not os.path.isdir(path):
+        raise ValueError(f"{path} is not a directory")
+    files = glob.glob(path + glob_str, recursive=True)
+    if not files:
+        raise ValueError(f"No jsonl files found in {path} using glob {glob_str}")
+    return files
+
+
+def load_multiple_datasets(paths: List[str]) -> Dict[str, List[DataSample]]:
+    """Load multiple datasets into a dictionary of lists of DataSamples
+
+    Args:
+        paths (List[str]): _description_
+
+    Returns:
+        Dict[str, List[DataSample]]: _description_
+    """
+    samples = {}
+    for file in tqdm(
+        paths,
+        leave=False,
+        desc="Loading jsonl files",
+        ncols=100,
+        unit="file",
+    ):
+        samples[file] = Dataset(file)
+    return samples
+
+
 class Dataset:
 
-    def __init__(self, path: str, glob_str: str = "/**/*.jsonl") -> None:
+    def __init__(self, path: str) -> None:
         """path should be a directory of directories of jsonl files
 
         Args:
             path (str): _description_
         """
         self.path = path
-        self.glob_str = glob_str
 
-        # load all jsonl files
-        path = os.path.normpath(path)  # format the path
-        if not os.path.isdir(path):
-            raise ValueError(f"{path} is not a directory")
-        self.files = glob.glob(path + glob_str, recursive=True)
-        if not self.files:
-            raise ValueError(f"No jsonl files found in {path} using glob {glob_str}")
-
-        # load all jsonl files, setup like the directory structure
-        self.samples = {}
-        for file in tqdm(
-            self.files,
-            leave=False,
-            desc="Loading jsonl files",
-            ncols=100,
-            unit="file",
-        ):
-            samples = Dataset.load_jsonl(file)
-            self.samples[file] = samples
-
-    @classmethod
-    def load_jsonl(cls, path: str) -> List[DataSample]:
-        """Load a jsonl file into a list of DataSamples
-
-        Args:
-            path (str): _description_
-
-        Returns:
-            List[DataSample]: _description_
-        """
         with open(path, "r") as f:
             lines = f.readlines()
-        samples = []
+
+        self.samples = []
         for line in lines:
             data = json.loads(line)
             sample = DataSample(
@@ -100,14 +111,26 @@ class Dataset:
                 rules=data["rules"] if "rules" in data else {},
                 abductions=data["abductions"] if "abductions" in data else {},
             )
-            samples.append(sample)
-        return samples
+            self.samples.append(sample)
+        self.samples
 
 
 if __name__ == "__main__":
-    path = "proofwriter-dataset/OWA/depth-2/meta-abduct-test.jsonl"
-    # dataset = Dataset.load_jsonl(path)
-    # pprint(dataset[0])
+    path = "proofwriter-dataset/OWA/depth-2/meta-test.jsonl"
+    top_dir = os.path.join("proofwriter-dataset", "OWA")
+    paths = get_files(top_dir, "/**/*.jsonl")
 
-    dataset = Dataset(os.path.join("proofwriter-dataset", "OWA"))
-    print(dataset.samples.keys())
+    paths = list(
+        filter(
+            lambda x: os.path.basename(x) == "meta-test.jsonl"
+            or os.path.basename(x) == "meta-train.jsonl"
+            or os.path.basename(x) == "meta-dev.jsonl",
+            paths,
+        )
+    )
+    print(paths)
+
+    # all_datasets = load_multiple_datasets(paths)
+
+    dataset = Dataset(path)
+    print(dataset.samples[0])

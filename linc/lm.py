8e5d352631f3b5d2785444c84813af4870d120d6
Rai <62800649+menamerai@users.noreply.github.com>
2024-03-17 20:04:39 -0400

    Neurosymbolic prompt (#9)
    * added prompt generation
    
    * modified project files
    
    * remodelled lm for better integration
    
    * added hf model
    
    * added gpu version
    
    * implemented stopping criteria for hfmodel
    
    * reformatted predict function
    
    * added prover9 logic prover
    
    * added support for if prover9 hits fatal error
    
    * added gemini api model
    
    * added cohere api model
    
    * added cohere key instructions
    
    * added quantization option
    
    * import conversion code from unified source
    
    * unified type location
    
    * added HF custom config
    
    * added api model config
    
    * made neurosymbolic prompt work
    
    * make neurosymbolic prompt work
    
    * corrected dependencies
    
    ---------
    
    Co-authored-by: Arnav Komaragiri <komaraac@mail.uc.edu>

--- linc/lm.py
+++ linc/lm.py
@@ -4,7 +4,7 @@ from typing import Optional
 import torch
 from custom_types import MODEL_MODE
 from transformers import AutoTokenizer, BitsAndBytesConfig, StoppingCriteria
-from utils import convert_to_nltk_rep
+from utils import bnb_factory, convert_to_nltk_rep
 
 
 class PromptGenerator:
@@ -35,7 +35,7 @@ class PromptGenerator:
             raise ValueError(f"Invalid mode: {mode}, expected one of {self.modes}")
 
         # get examples
-        if self.n_shots > 0 and mode == MODEL_MODE.BASELINE:
+        if self.n_shots > 0:
             prompt += self.get_and_format_example(mode)
 
         # write frame for problem
@@ -65,9 +65,12 @@ class PromptGenerator:
             if mode == MODEL_MODE.BASELINE:
                 formatted_examples += example["label"] + "\n"
             elif mode == MODEL_MODE.NEUROSYMBOLIC:
-                for premise, fol in zip(examples["premises"], examples["premises_FOL"]):
+                for premise, fol in zip(example["premises"], example["premises_FOL"]):
                     formatted_examples += f"TEXT: {premise}\n"
                     formatted_examples += f"FOL: {fol}\n"
+
+                formatted_examples += f"TEXT: {example['conclusion']}\n"
+                formatted_examples += f"FOL: {example['conclusion_FOL']}\n"
             formatted_examples += f"</EVALUATE>\n\n"
 
         return formatted_examples
@@ -87,6 +90,7 @@ class PromptGenerator:
                     "In La Liga 2021-2022, Real Madrid and Barcelona both recieve 3 points from the games between them.",
                 ],
                 "conclusion": "In La Liga 2021-2022, Real Madrid ranks higher than Barcelona.",
+                "conclusion_FOL": "HigherRank(RealMadrid, Barcelona)",
                 "label": "True",
                 "premises_FOL": [
                     "∀x ∀y (LaLiga(x) ∧ LaLiga(y) ∧ MorePoints(x, y) → HigherRank(x, y))",
@@ -106,6 +110,7 @@ class PromptGenerator:
                     "If Amy is not a Nobel laureate, then Amy is not an Olympic gold medal winner.",
                 ],
                 "conclusion": "If Amy is not an Olympic gold medal winner, then Amy is a Nobel laureate.",
+                "conclusion_FOL": "-OlympicGoldMedalWinner(Amy) -> NobelLaureate(Amy)",
                 "label": "False",
                 "premises_FOL": [
                     "∀x (Athlete(x) → GoodAtSports(x))",
@@ -125,7 +130,8 @@ class PromptGenerator:
                     "All environment-friendly things are good.",
                     "A worksheet is either paper or is environment-friendly.",
                 ],
-                "conclusion": "A worksheet is dispensable.",
+                "conclusion": "A worksheet is not dispensable.",
+                "conclusion_FOL": "-Dispensable(worksheet)",
                 "label": "Uncertain",
                 "premises_FOL": [
                     "∀x (Dispensable(x) → EnvironmentFriendly(x))",
@@ -141,8 +147,11 @@ class PromptGenerator:
         if mode == MODEL_MODE.NEUROSYMBOLIC:
             for example in examples:
                 example["premises_FOL"] = [
-                    convert_to_nltk_rep(premise) for premise in example["premises"]
+                    convert_to_nltk_rep(premise) for premise in example["premises_FOL"]
                 ]
+                example["conclusion_FOL"] = convert_to_nltk_rep(
+                    example["conclusion_FOL"]
+                )
 
         return examples[:n]
 
@@ -174,14 +183,6 @@ class StopOnWords(StoppingCriteria):
             if torch.equal(input_ids[0][-len(stop_token_id) :], stop_token_id):
                 return True
         return False
-
-def bnb_factory():
-    return BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16,
-        )
 
 
 @dataclass
@@ -284,5 +285,33 @@ class CohereModelConfig:
 
 if __name__ == "__main__":
     prompt_gen = PromptGenerator()
-    prompt = prompt_gen.get_examples(MODEL_MODE.NEUROSYMBOLIC, 3)
+    example_doc = {
+        "premises": [
+            "If a city hold a Summer Olympics, and the city is a US city, then the Summer Olympics will be in the US.",
+            "If a city is in a state which is in US, the city is a US city.",
+            "If a city in a state, and a Summer Olympics is in this city, then the Summer Olympics is in this state.",
+            "The 2028 Summer Olympics is scheduled to take place in Los Angeles(LA).",
+            "LA is a city in California(CA).",
+            "Atlanta is a US city.",
+            "Atlanta is in Georgia(CA).",
+            "CA is a state in the United States.",
+            "Boxing, modern pentathlon, and weightlifting will be removed from The 2028 Summer Olympics.",
+            "Atlanta in the United States hold the 1996 Summer Olympics.",
+        ],
+        "conclusion": "The 2028 Summer Olympics will take place in the US.",
+        "premises_FOL": [
+            "∀x ∀y (SummerOlympicsCity(x, y) ∧ CityInCountry(y, us) → SummerOlympicsCountry(x, us))",
+            "∀x ∀y (CityInState(x, y) ∧ StateInCountry(y, us) → CityInCountry(x, us))",
+            "∀x ∀y ∀z (CityInState(x, y) ∧ SummerOlympicsCity(z, x) → SummerOlympicsState(z, y))",
+            "SummerOlympicsCity(y2028, la)",
+            "CityInState(la, ca)",
+            "CityInCountry(atlanta, us)",
+            "StateInCountry(ca, us)",
+            "CityInState(atlanta, ga)",
+            "¬InSummerOlympics(y2028, boxing) ∧ ¬InSummerOlympics(y2028, modern_pentathlon) ∧ ¬InSummerOlympics(y2028, weightlifting)",
+            "SummerOlympicsCity(y1996, atlanta)",
+        ],
+        # "label": "True",
+    }
+    prompt = prompt_gen.generate(MODEL_MODE.NEUROSYMBOLIC, example_doc)
     print(prompt)

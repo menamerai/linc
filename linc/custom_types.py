f970a207f307faeb7979b8fddfdc06b847de3352
Arnav Komaragiri <komaraac@mail.uc.edu>
2024-03-18 08:57:08 -0400

    added ordering on OWA_PRED type to allow for np.unique() to do its thing

--- linc/custom_types.py
+++ linc/custom_types.py
@@ -1,4 +1,16 @@
 from enum import Enum
+from functools import total_ordering
 
-OWA_PRED = Enum("PRED", ["FALSE", "TRUE", "UNK"])
+@total_ordering
+class OWA_PRED(Enum):
+    FALSE = 0
+    TRUE = 1
+    UNK = 2
+    ERR = 3
+    
+    def __lt__(self, a):
+        if type(self) == type(a):
+            return self.value < a.value
+        raise ValueError(f"cannot compare enum to type {type(a)}")
+
 MODEL_MODE = Enum("MODEL_MODE", ["BASELINE", "NEUROSYMBOLIC"])
